8d7ee98ec6030592f33598a2d1a0c23fff678a8e
Spectacular2001 <ruchitadhore2001@gmail.com>
2024-10-30 22:05:17 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static main(String[]args){
+      RWonly obj =  new RWonly();
+      obj.setName("Test Name");
+      System.out.println(obj.getName());
+    }
 }
