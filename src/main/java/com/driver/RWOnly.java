30d504be28356cf8bfafcdae21f6eb30f680f100
Spectacular2001 <ruchitadhore2001@gmail.com>
2024-10-30 21:57:35 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
+    privete String name;
+    public String getNa(){
+        return name;
     
 }
+    public void setNam(String name){
+        this.name + name;
+    }
